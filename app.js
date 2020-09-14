54f1a346bd1ae3a6183a9dccf2e3d1f9efd86dd9
clover243 <amyshao15@gmail.com>
2020-09-13 19:44:40 -0700

    nav done

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-const navSlide = () -> {
+
+const navSlide = () => {
     const menu = document.querySelector('.menu');
     const nav = document.querySelector('.nav-links');
-
-    menu.addEvent
+    const navlinks = document.querySelectorAll('.nav-links li');
+    
+    
+    menu.addEventListener('click', () => {
+        // toggle nav bar
+        if (nav.style.transition) {
+            nav.classList.toggle('nav-active');
+            nav.style.transition = "";
+        } else {
+            nav.style.transition = `transform 0.5s ease-in-out`;
+            nav.classList.toggle('nav-active');
+        }
+        //nav.style.transition = `transform 0.5s ease-in-out`;
+        
+        // animate nav links
+        navlinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navfade 1s ease forwards ${index/8 }s`;
+            }
+            
+        });
+    });
     
 }
+
+navSlide();
